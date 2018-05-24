ae589542f043692e2a508de1a4d979d92805b015
wasuthun wanaphongthipakorn <pond_Zen@hotmail.com>
2018-05-24 20:49:16 +0700

    Remove some duplicate

--- app/src/main/java/com/example/johnpaul/harty/Registation.kt
+++ app/src/main/java/com/example/johnpaul/harty/Registation.kt
@@ -24,9 +24,4 @@ class Registation: AppCompatActivity() {
         view.context.startActivity(intent)
 
     }
-    public fun getMap(): HashMap<String, String> {
-        return this.map
-    }
-
-
 }
